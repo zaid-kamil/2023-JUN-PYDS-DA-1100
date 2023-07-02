7c0bd35e74ad3a05d651c742dad9702fb06fd66d
zaid <xaidmetamorphos@gmail.com>
2023-07-02 12:35:48 +0530

    conditions

---
+++ t1.py
@@ -0,0 +1,8 @@
+from turtle import *
+speed('slowest') # slowest, slow, normal, fast, fastest
+for i in range(10):
+    fd(100)
+    rt(360/10)
+hideturtle() # hide the turtle
+mainloop() # hold the window open
+
