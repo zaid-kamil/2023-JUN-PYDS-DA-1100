2f426a6aa2ee3e3112b163c0c2903a771885b66e
zaid <xaidmetamorphos@gmail.com>
2023-07-26 12:27:15 +0530

    working code #1

--- game2.py
+++ game2.py
@@ -7,6 +7,11 @@ HEIGHT = 500
 class Player(Actor):
     '''This is class Player, that inherits from Actor'''
     speed = 5
+
+    def __init__(self, image):
+        super().__init__(image)
+        self.pos = (WIDTH/2, HEIGHT/2) 
+
     def movement(self):
         '''This is method movement, that controls player movement'''
         if keyboard.left:
@@ -22,6 +27,10 @@ class Player(Actor):
 
 class Enemy(Actor):
     speed = 2
+    def __init__(self, image):
+        super().__init__(image) # call parent class constructor
+        self.pos = (-100, HEIGHT/2) # set initial position
+
     def tracking(self, p):
         # enemy tracks player
         if p.x > self.x:
@@ -37,10 +46,33 @@ class Enemy(Actor):
             exit()
 
 class Fruit(Actor):
-    x = randint(50, WIDTH-50)
-    y = randint(50, HEIGHT-50)
+    
+    def __init__(self, image):
+        super().__init__(image)
+        x = randint(50, WIDTH-50)
+        y = randint(50, HEIGHT-50)
 
     def relocate(self):
         self.x = randint(50, WIDTH-50)
         self.y = randint(50, HEIGHT-50)
+
+# game code start now
+
+p = Player('hero')
+e = Enemy('enemy')
+c = Fruit('fruit')  
+score = 0
+
+def draw():
+    screen.clear()
+    p.draw()
+    e.draw()
+    c.draw()
+    screen.draw.text(f'Score: {score}', (10, 10), color='white')
+
+def update():
+    p.movement()
+    e.tracking(p)
+
+pgzrun.go()
 
