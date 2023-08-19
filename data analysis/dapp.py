e2eaf94230f4af0aba7bbd92a75b9998079e091a
zaid kamil <xaidmetamorphos@gmail.com>
2023-08-19 14:36:14 +0530

    pandas

---
+++ data analysis/dapp.py
@@ -0,0 +1,11 @@
+import streamlit as st
+# run in terminal
+# cd data analysis
+# streamlit run dapp.py
+
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import seaborn as sns
+
+st.title('Data Analysis App')
